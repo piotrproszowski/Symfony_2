0a0432250b30a3a6433c2c0ae8775e825ad68333
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2025-04-30 23:19:02 +0200

    feat(hello): add HelloController and corresponding Twig template

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/hello')]
+class HelloController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->render('hello/index.html.twig', [
+            'controller_name' => 'HelloController',
+        ]);
+    }
+
+    public fuibcrtiob
+    
+}
