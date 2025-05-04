1317da58d9210b1a28e87963a860dcf04f2c82ad
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2025-05-04 02:14:30 +0200

    feat(micro-post): add MicroPost entity, repository, and migration for micro_post table

---
+++ src/Entity/MicroPost.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MicroPostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MicroPostRepository::class)]
+class MicroPost
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer', nullable: false, unique: true)]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 500, nullable: true)]
+    private ?string $text = null;
+
+    #[ORM\Column]
+    private ?\DateTime $datetime = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): static
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getDatetime(): ?\DateTime
+    {
+        return $this->datetime;
+    }
+
+    public function setDatetime(\DateTime $datetime): static
+    {
+        $this->datetime = $datetime;
+
+        return $this;
+    }
+}
